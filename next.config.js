ec0c4d43c72b16db4f2e716f60afdb266ab4ec76
Your Name <you@example.com>
2018-10-29 19:17:23 -0500

    Calculator deploy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   // some configuration
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/{React_Calculator}' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/React_Calculator' : '',
   // another configuration
 }
